a9a7c9853ae0f1dbc12110cb266a9fb2f2dabb0b
Salohy <salohiniainarak@gmail.com>
2025-11-10 22:26:11 +0300

    Adjust elements to fit the UI figma.

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const menuContainer = document.querySelector("#menu-container");
 const closeButton = document.querySelector("#close");
 const home = document.querySelector('#home-caption');
 const homeIcon = document.querySelector('#home-icon');
+const fram559 = document.querySelector('#fram-559');
+const heading  = document.querySelector('#heading');
 
 image.addEventListener('click', ()=>{
     menuActive.style.display = "block";
     menuContainer.style.display = "block";
+    heading.style.height = '38px';
 });
 
 closeButton.addEventListener('click', ()=>{
     menuActive.style.display = "none";
     menuContainer.style.display = "none";
+    heading.style.height = '54.62px';
 });
 
 home.addEventListener('click', ()=>{
